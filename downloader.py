ff50dba1ae1157ec0b76bdeb8f6dbc7d27c27bdd
kenken <kentaroy@stanford.edu>
2018-12-12 11:44:19 -0800

    Update downloader.py
    added URL variable

--- downloader.py
+++ downloader.py
@@ -4,16 +4,21 @@ import time
 from apscheduler.scheduler import Scheduler
 import subprocess
 
+# target URL
+URL = 'https://www.youtube.com/watch?v=K-F4CeVsWHA'
+
 # Start the scheduler
 sched = Scheduler()
 sched.daemonic = False
 sched.start()
 
+# define Job
 def job_function():
     now = datetime.datetime.now()
-    command='streamlink https://www.youtube.com/watch?v=K-F4CeVsWHA 720p --hls-start-offset 01:00:00 --hls-duration 01:00:00 -o "'+str(now.year)+'-'+str(now.month)+'-'+str(now.day)+'-'+str(now.hour)+str(now.minute)+'-jackson.ts"'
+    ### CHANGE THE URL HERE ####
+    command='streamlink ' + URL + ' 720p --hls-start-offset 01:00:00 --hls-duration 01:00:00 -o "'+str(now.year)+'-'+str(now.month)+'-'+str(now.day)+'-'+str(now.hour)+str(now.minute)+'-jackson.ts"'
     subprocess.call(command,shell=True)
     time.sleep(20)
 
-# Schedules job_function to be run once each minute
+# Schedules job_function to be run once each hour
 sched.add_cron_job(job_function, hour='0-23')
